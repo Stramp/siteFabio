5af453907346eaaa30fe48373b5513fe26a02941
Stramp <rafaelcostart2@gmail.com>
2020-09-26 03:03:12 -0300

    Configurando o ESlint

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Fabio Morelli Trainer</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+teste
+
+    </div>
+  )
+}
