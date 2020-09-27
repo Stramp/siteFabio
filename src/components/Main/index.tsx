08f9d172faafd47bd14572d9ca5a51c89b1c8947
Stramp <rafaelcostart2@gmail.com>
2020-09-26 21:31:53 -0300

    github actions3

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,6 +1,6 @@
 import * as S from './styles'
 
-const Main = ({ title = 'Fabio Morelli', description = 'descrição' }) => (
+const Main = ({ title = 'Fabio Morelli', description = 'descrição e ae' }) => (
   <S.Wrapper>
     <h1>{title}</h1>
     <S.Descriptions>{description}</S.Descriptions>
