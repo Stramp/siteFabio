52ef40af2dc0cb61d4a4b927a6e3f27675672449
Rafael <rafaelcostart2@gmail.com>
2021-06-06 22:32:12 -0300

    reinstall husky3

--- src/components/Teste/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Meta, Story } from '@storybook/react/types-6-0'
-import Teste, { TesteProps } from '.'
-
-export default {
-  title: 'Teste',
-  component: Teste
-} as Meta
-
-export const Default: Story<TesteProps> = (args) => (<Teste {...args} />)
--- src/components/Teste/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled, { css } from 'styled-components'
-import { TesteProps } from '.'
-
-export const Wrapper = styled.main<TesteProps>`
-  ${({ theme }) => css`
-    color: ${theme.colors.mainTxt};
-  `}
-`
