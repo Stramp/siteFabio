04b06943e2d6ed04ad9227cbd1600f20ca264506
rafael <rafaelcostart2@gmail.com>
2021-07-04 23:43:27 -0300

    fix storie boook

--- src/components/sections/SectionContact/stories.tsx
+++ src/components/sections/SectionContact/stories.tsx
@@ -1,11 +1,9 @@
 import { Meta, Story } from '@storybook/react/types-6-0'
-import SectionContact, { SectionContactProps } from '.'
+import SectionContact from '.'
 
 export default {
   title: 'SectionContact',
   component: SectionContact
 } as Meta
 
-export const Default: Story<SectionContactProps> = (args) => (
-  <SectionContact {...args} />
-)
+export const Default: Story = (args) => <SectionContact {...args} />
